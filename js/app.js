bbcaf5ed14ea3824b7156e7693db17b00cfa211e
akiiin44 <akintek@outlook.de>
2023-04-10 00:30:51 +0200

    v1.4.1
    iPad Accordion fix

--- js/app.js
+++ js/app.js
@@ -5,38 +5,22 @@ duaElements.forEach((dua) => {
   const duaToggle = dua.querySelector('.dua-toggle');
   const duaContent = dua.querySelector('.dua-content');
 
-  duaHeader.addEventListener('click', () => {
+  // Eine Funktion, die das Ein- und Ausklappen steuert
+  function toggleDua() {
     dua.classList.toggle('dua-active');
 
     if (dua.classList.contains('dua-active')) {
       duaToggle.innerHTML = '<i class="fas fa-minus"></i>';
+      // Die Höhe des Inhalts dynamisch anpassen
+      const contentHeight = duaContent.scrollHeight;
+      duaContent.style.maxHeight = contentHeight + 'px';
     } else {
       duaToggle.innerHTML = '<i class="fas fa-plus"></i>';
+      // Die Höhe des Inhalts auf null setzen
+      duaContent.style.maxHeight = '0px';
     }
-  });
+  }
+
+  // Nur einen Event-Listener pro Element hinzufügen
+  duaHeader.addEventListener('click', toggleDua);
 });
-
-
-const duaHeader = document.querySelector('.dua-header');
-
-duaHeader.addEventListener('click', function() {
-  const duaContent = this.nextElementSibling;
-  const duaToggle = this.querySelector('.dua-toggle');
-  
-  duaContent.classList.toggle('open');
-  duaToggle.classList.toggle('open');
-});
-
-const duaHeaders = document.querySelectorAll('.dua-header');
-
-duaHeaders.forEach(function(header) {
-  header.addEventListener('click', function() {
-    const duaContent = this.nextElementSibling;
-    const duaToggle = this.querySelector('.dua-toggle');
-    const contentHeight = duaContent.scrollHeight;
-    
-    duaContent.style.maxHeight = contentHeight + 'px';
-    duaContent.classList.toggle('open');
-    duaToggle.classList.toggle('open');
-  });
-})
